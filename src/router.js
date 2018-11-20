9166faa62f62df385488b86d69fa9616f2c01ae5
ameentalahmeh <ayman201266@hotmail.com>
2018-11-20 12:51:28 +0200

    First three endpoint done #3

---
+++ src/router.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const path = require('path');
+const handlers = require('./handlers.js');
+
+const router = (request, response)=> {
+
+  if (request.url === '/') {
+        handlers.homeHandler(request,response)
+  }
+//-----------------------------------------------------------------------
+else if (request.url.includes('/public/')) {
+        handlers.publicHandler(request,response)
+}
+//-----------------------------------------------------------------------
+// error-----------------------------------------------------------------
+else{
+    response.writeHead(404,{'Content-Type': 'text/html'})
+    response.end('<h1> Page Not Found </h1>')
+  }
+
+}
+  module.exports = router
